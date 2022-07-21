19426d1289263f11a5231262be761180b4a5793d
Feras <ferassaleh1980@gmail.com>
2022-07-21 10:46:47 +0200

    add some functions to the code and finishing the project

--- index.js
+++ index.js
@@ -1,74 +1,123 @@
-let display = document.querySelector('.display');
-let secondaryDisplay = document.querySelector('.secondary-display');
-const keysPad = document.querySelector('.calculator-keys');
+const display = document.querySelector('.display');
+const secondaryDisplay = document.querySelector('.secondary-display');
+const btnZero = document.querySelector('.zero');
+const btnOne = document.querySelector('.one');
+const btnTwo = document.querySelector('.two');
+const btnThree = document.querySelector('.three');
+const btnFour = document.querySelector('.four');
+const btnFive = document.querySelector('.five');
+const btnSix = document.querySelector('.six');
+const btnSeven = document.querySelector('.seven');
+const btnEight = document.querySelector('.eight');
+const btnNine = document.querySelector('.nine');
+const btnComma = document.querySelector('.comma');
+const btnPlus = document.querySelector('.plus');
+const btnMinus = document.querySelector('.minus');
+const btnTimes = document.querySelector('.times');
+const btnDivide = document.querySelector('.divide');
+const btnEqual = document.querySelector('.equal');
+const btnDelete = document.querySelector('.delete');
+
 let operator ='';
-let firstOperand = null;
+let firstOperand = 0;
 let secondOperand = null;
-let finalResult = null;
+let finalResult = 0;
+let displyedvalues = '0';
 
-
-const getResult = () => {
-    if (operator === '/') {
-        finalResult = firstOperand / secondOperand;
-        console.log(finalResult);
-        display.textContent = finalResult.toString();
-    } else if (operator === '+') {
-        finalResult = firstOperand + secondOperand;
-        console.log(finalResult);
-        display.textContent = finalResult.toString();
-    } else if (operator === '-') {
-        finalResult = firstOperand - secondOperand;
-        console.log(finalResult);
-        display.textContent = finalResult.toString();
-    } else if (operator === '*') {
-        finalResult = firstOperand * secondOperand;
-        console.log(finalResult);
-        display.textContent = finalResult.toString();
-    } 
-}
-
-keysPad.addEventListener('click', (e) => {
-    const key = e.target;
-    const keyValue = key.textContent;
-    const displayedValue = display.textContent;
-    secondaryDisplay.textContent += key.textContent;
-
-    // check if the pressed key is a number
-
-    if (key.dataset.type ==='number') {
-        if (displayedValue === '0') {
-            display.textContent = keyValue;
+const addToSecondDisplay = ({newText, removeLastChart, reset}) => {
+        if ( reset || (displyedvalues === '0' && typeof newText === 'number')) {
+            displyedvalues = newText;
         } else {
-            display.textContent = displayedValue + keyValue;
+            if ( removeLastChart) {
+                displyedvalues = displyedvalues.slice(0, -1);
+            }
+            displyedvalues += newText;
+
         }
+    secondaryDisplay.textContent = displyedvalues;
+} 
+const addNumber =  (num) => {
+    addToSecondDisplay({newText: num});
+    if (operator) {
+        secondOperand = secondOperand * 10 + num; 
+    } else {
+        firstOperand = firstOperand * 10 + num;
     }
-    
-    // check if the pressed key is an operator
+}
+const calculate = () => {
+    if (operator === '+') {
+        return firstOperand + secondOperand;
+    }  
+    if (operator === '-') {
+        return firstOperand - secondOperand;
+    }
+    if (operator === '*') {
+        return firstOperand * secondOperand;
+    }
+    if (operator === '/') {
+        return firstOperand / secondOperand;
+    }
+}
+const addOperator = (newOperator) => {
+    if (operator) {
+        if (secondOperand !== null) {
+            firstOperand = calculate();
+            secondOperand = null;
+            operator = newOperator;
+            addToSecondDisplay({reset: true, newText: firstOperand + newOperator })
 
-    if (key.dataset.type === 'operator' && key.textContent !== '=') {
-        firstOperand = parseFloat(display.textContent);
-        console.log(firstOperand);
-        operator = key.textContent;
-       // if (operator !== '') {
-       //     display.textContent = '';
-       //     return;
-       // }
+        } else {
+            operator = newOperator;
+            addToSecondDisplay({newText: newOperator,removeLastChart: true})
+        }
+    } else {
+        operator = newOperator;
+        addToSecondDisplay({newText : newOperator});
     }
 
-    if (operator !== '' && key.dataset.type ==='number') {
-        secondOperand = parseFloat(display.textContent);
-        console.log(secondOperand);
-        
+}
+btnOne.addEventListener('click', () => addNumber(1));
+btnTwo.addEventListener('click', () => addNumber(2));
+btnThree.addEventListener('click', () => addNumber(3));
+btnFour.addEventListener('click', () => addNumber(4));
+btnFive.addEventListener('click', () => addNumber(5));
+btnSix.addEventListener('click', () => addNumber(6));
+btnSeven.addEventListener('click', () => addNumber(7));
+btnEight.addEventListener('click', () => addNumber(8));
+btnNine.addEventListener('click', () => addNumber(9));
+btnZero.addEventListener('click', () => addNumber(0));
+btnComma.addEventListener('click', () => addNumber('.'));
+btnTimes.addEventListener('click', () => addOperator('*'));
+btnDivide.addEventListener('click', () => addOperator('/'));
+btnMinus.addEventListener('click', () => addOperator('-'));
+btnPlus.addEventListener('click', () => addOperator('+'));
+btnEqual.addEventListener('click', () => {
+    if (!operator) {
+        return;
     }
-
-    if (key.textContent === 'AC') {
-        display.textContent = '0';
-        secondaryDisplay.textContent = '';
-    }
-
-    if (key.textContent === '=') {
-        console.log(key.textContent);
-        getResult();
-    }
+    firstOperand = calculate();
+    secondOperand = null;
+    operator = '';
+    addToSecondDisplay({reset: true, newText: firstOperand});
+    display.textContent = firstOperand;
 });
-console.log(typeof(secondaryDisplay.textContent));
+btnDelete.addEventListener('click', () => {
+    firstOperand = 0;
+    secondOperand = null;
+    operator = '';
+    addToSecondDisplay({reset: true, newText: 0});
+    display.textContent = '';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
